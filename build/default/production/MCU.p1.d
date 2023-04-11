0efa34e602a74c70ccd9ad093c384edc7b51c75b
DO VAN HAU <118800101+dovanhau1999@users.noreply.github.com>
2023-04-11 08:09:27 +0700

    update
    thêm

---
+++ build/default/production/MCU.p1.d
@@ -0,0 +1,9 @@
+build/default/production/MCU.p1:  \
+MCU.c  \
+MCU.h  \
+I2C_SHT30.h  \
+Modbus_Slave.h  \
+ModbusRTU/ModbusRTU.h  \
+ModbusRTU/../Modbus.h  \
+tick.h  \
+mcc_generated_files/eusart.h 
---
+++ build/default/production/Mobbus_Slave.p1.d
@@ -0,0 +1,8 @@
+build/default/production/Mobbus_Slave.p1:  \
+Mobbus_Slave.c  \
+Modbus_Slave.h  \
+ModbusRTU/ModbusRTU.h  \
+ModbusRTU/../Modbus.h  \
+tick.h  \
+mcc_generated_files/eusart.h  \
+mcc_generated_files/pin_manager.h 
